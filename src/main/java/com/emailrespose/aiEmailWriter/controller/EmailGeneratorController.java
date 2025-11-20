256dbe045d550bf5da20d3a0758732d311760a37
Arun Kumar Soni <120294411+ArunSoni01@users.noreply.github.com>
2025-11-20 21:11:03 +0530

    initial commit

---
+++ src/main/java/com/emailrespose/aiEmailWriter/controller/EmailGeneratorController.java
@@ -0,0 +1,22 @@
+package com.emailrespose.aiEmailWriter.controller;
+
+import com.emailrespose.aiEmailWriter.dto.EmailRequest;
+import com.emailrespose.aiEmailWriter.service.EmailGeneratorService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/email")
+@AllArgsConstructor
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
